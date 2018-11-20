bed215d9b94bd1984166b528cd502bdfbad41e48
jenew <jenew@h2018>
2018-11-20 14:55:50 +0900

    BizService

--- src/main/java/com/hami/sys/support/BizService.java
+++ src/main/java/com/hami/sys/support/BizService.java
@@ -1,5 +1,8 @@
 package com.hami.sys.support;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,4 +19,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class BizService {
     public final Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getSchParam(Map<String, Object> paramMap) {
+        
+        Map<String, Object> searchMap = new HashMap<String, Object>();
+        if(!searchMap.isEmpty() && searchMap.containsKey("ds_search")){
+            searchMap = (Map<String, Object>)paramMap.get("ds_search");
+        } else {
+            searchMap = paramMap;
+        }
+        return searchMap;
+    }
 }
